d3129e7b7815dbab0aef1e87b72d9073319384a5
Ahsan Iqbal <mianahson@gmail.com>
2019-09-19 23:54:31 +0200

    adds sampling functionality

--- Models/LSTMBased.py
+++ Models/LSTMBased.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.distributions import Categorical
 
 
 class LSTMModel(nn.Module):
@@ -8,8 +9,16 @@ class LSTMModel(nn.Module):
 
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size)
         self.fc = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, X):
         outputs, _ = self.lstm(X)
         outputs = self.fc(outputs.view(-1, outputs.shape[-1]))
         return outputs
+
+    def sample(self, X):
+        outputs, _ = self.lstm(X)
+        outputs = self.fc(outputs.view(-1, outputs.shape[-1]))
+        m = Categorical(probs=self.softmax(outputs[-1].view(1, -1)))
+        return m.sample()
+
